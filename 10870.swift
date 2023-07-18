5a2e1e9bd559d50c759edccc8f84af63ab7a341f
Louie <94155642+woojinchoi17@users.noreply.github.com>
2023-07-18 19:21:50 +0900

    BOJ10870

---
+++ 10870.swift
@@ -0,0 +1,8 @@
+let n = Int(readLine()!)!
+
+func fibonacci(_ n: Int)-> Int {
+    if n == 0 { return 0 }
+    if n == 1 { return 1 }
+    return fibonacci(n-1)+fibonacci(n-2)
+}
+print(fibonacci(n))
