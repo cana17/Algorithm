29675fee692282cfd212e7f8872086934a410610
Louie <94155642+woojinchoi17@users.noreply.github.com>
2023-07-28 09:57:11 +0900

    BOJ11022

---
+++ 11022.swift
@@ -0,0 +1,6 @@
+let t = Int(readLine()!)!
+for i in 1...t {
+    let input = readLine()!.split(separator: " ").map{ Int($0)! }
+    let sum = String(input[0]+input[1])
+    print("Case #\(i): \(input[0]) + \(input[1]) = \(sum)")
+}
