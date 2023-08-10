9e4ef50b499d11a66ed391a5a43540df1c9784a7
Louie <94155642+woojinchoi17@users.noreply.github.com>
2023-08-11 08:52:21 +0900

    BOJ11651

--- 11651.swift
+++ 11651.swift
@@ -1,18 +1,11 @@
-let input = readLine()!.split(separator: " ").map { Int($0)! }
-var arr = Array(1...input[0])
+let n = Int(readLine()!)!
+var coordinate: [(x: Int, y: Int)] = []
 
-for _ in 0..<input[1] {
-    let num = readLine()!.split(separator: " ").map { Int($0)! }
-    var i = num[0] - 1
-    var j = num[1] - 1
-
-    // 주어진 범위의 배열을 역순으로 만드는 코드
-    while i < j {
-        arr.swapAt(i, j)
-        i += 1
-        j -= 1
-    }
+for _ in 0..<n {
+    let input = readLine()!.split(separator: " ")
+    let a = Int(input[0])!
+    let b = Int(input[1])!
+    coordinate.append((a, b))
 }
-
-let result = arr.map { String($0) }.joined(separator: " ")
-print(result)
+coordinate.sort { $0.1 == $1.1 ? $0.0 < $1.0 : $0.1 < $1.1 } // y좌표 같으면 x좌표 증가순으로
+coordinate.forEach{print($0.x, $0.y)}
