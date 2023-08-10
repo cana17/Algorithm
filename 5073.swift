f4740f0806d58ac6b3f2d675c9f998f10579317f
Louie <94155642+woojinchoi17@users.noreply.github.com>
2023-08-10 12:33:02 +0900

    BOJ5073

---
+++ 5073.swift
@@ -0,0 +1,16 @@
+while true {
+    let n = readLine()!.split(separator: " ").map { Int($0)! }.sorted()
+    
+    if n == [0, 0, 0] { break }
+    
+    if n[0]+n[1] <= n[2] { print("Invalid") }
+    else {
+        if n[0]==n[1] && n[0] == n[2] && n[1] == n[2] {
+            print("Equilateral")
+        } else if n[0]==n[1] || n[0]==n[2] || n[1]==n[2] {
+            print("Isosceles")
+        } else {
+            print("Scalene")
+        }
+    }
+}
