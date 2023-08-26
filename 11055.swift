4dc4976da4f6244b664f52593923fb983e00b391
견우 <94155642+kyunwoo77@users.noreply.github.com>
2023-08-26 16:47:28 +0900

    BOJ11055

---
+++ 11055.swift
@@ -0,0 +1,16 @@
+let n = Int(readLine()!)!
+let arr = readLine()!.split(separator: " ").map{ Int($0)! }
+var dp = Array(repeating: 0, count: n)
+var maxSum = 0
+
+for i in 0..<n {
+    dp[i] = arr[i]
+    for j in 0..<i {
+        if arr[j] < arr[i] {
+            dp[i] = max(dp[i], dp[j] + arr[i])
+        }
+    }
+    maxSum = max(maxSum, dp[i])
+}
+
+print(dp.max()!)
