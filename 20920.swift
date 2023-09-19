7a1f6d600e99503fea3cfc1871b8be3f8fe39c17
광야의 개발자 <94155642+cana17@users.noreply.github.com>
2023-09-19 15:18:23 +0900

    BOJ20920

--- 20920.swift
+++ 20920.swift
@@ -15,7 +15,7 @@ let sortedVoca = voca.sorted {
     } else if $0.key.count != $1.key.count {
         return $0.key.count > $1.key.count //2. 해당 단어의 길이가 길수록 앞에 배치한다. -> value 같으면 단어가 더 길면 우선
     } else {
-        return $0.key < $1.key //3. 알파벳 사전 순으로 앞에 있는 단어일수록 앞에 배치한다. -> 길이 같으면 사전순으로 정렬 -> sort
+        return $0.key < $1.key //3. 알파벳 사전 순으로 앞에 있는 단어일수록 앞에 배치한다. -> 길이 같으면 사전순으로 정렬
     }
 }
 sortedVoca.forEach{ print($0.key) }
