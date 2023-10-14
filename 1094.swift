afcce8f89a0b456b727fb02d9585e1c55c0ab53f
cana17 <94155642+cana17@users.noreply.github.com>
2023-10-14 11:42:34 +0900

    BOJ1094

---
+++ 1094.swift
@@ -0,0 +1,17 @@
+print(Int(readLine()!)!.nonzeroBitCount)
+
+// let x = Int(readLine()!)!
+// let binaryX = String(x, radix: 2)
+
+// var oneCount = 0
+
+// for i in binaryX {
+//     if i == "1" {
+//         oneCount += 1
+//     }
+// }
+// print(oneCount)
+// 숫자에 맞는 전구 수를 세면 되는문제
+// x를 2진수로 변환 -> 1의 개수를 센다
+
+// print(x.nonzeroBitCount)
