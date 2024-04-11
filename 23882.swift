139912bacd369f9acd1ed41e800526d7867475f6
cana <94155642+canaios@users.noreply.github.com>
2024-04-11 20:46:33 +0900

    BOJ23882 선택정렬 문제2

---
+++ 23882.swift
@@ -0,0 +1,26 @@
+let input = readLine()!.split(separator: " ").map{ Int($0)! }
+let (n, k) = (input[0], input[1])
+
+var arr = readLine()!.split(separator: " ").map{ Int($0)! }
+var cnt = 0
+
+for i in stride(from: n-1, to: 0, by: -1) {
+    var maxIndex = 0
+
+    for j in 0...i {
+        if arr[maxIndex] < arr[j] {
+            maxIndex = j
+        }
+    }
+    if i != maxIndex {
+        arr.swapAt(i, maxIndex)
+        cnt += 1
+        if cnt == k {
+            print(arr.map{ String($0) }.joined(separator: " "))
+            break
+        }
+    }
+}
+if cnt < k {
+    print(-1)
+}
